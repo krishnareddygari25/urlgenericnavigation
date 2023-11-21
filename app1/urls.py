a9b9145b7de71568d3705c3dbc87da7abec8733b
krishnareddygari25 <krishnareddygarianusha@gmail.com>
2023-11-21 21:26:59 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,6 @@
+from app1.views import *
+from django.urls import path
+app_name='life'
+urlpatterns=[
+    path('anusha/',anusha,name='anusha'),
+]
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anusha(request):
+    return render(request,'anusha.html')
